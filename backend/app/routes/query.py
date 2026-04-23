6d91319494d277d4ae0a441209679791da78d198
Pranav Patil <pranavpatil9765@gmail.com>
2026-04-23 20:59:27 +0530

    added streaming responses

--- backend/app/routes/query.py
+++ backend/app/routes/query.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 
 from app.db.vector_store import get_vector_store
 from app.services.retrieval_service import retrieve_docs
-from app.services.llm_service import generate_answer
-
+from app.services.llm_service import stream_answer
+from fastapi.responses import StreamingResponse
 router = APIRouter()
 
 class QueryRequest(BaseModel):
@@ -19,14 +19,8 @@ def query_docs(request: QueryRequest):
         return {"error": "No documents uploaded"}
 
     docs = retrieve_docs(db, request.query)
-    sources = [
-        f"{doc.metadata.get('source', 'unknown')} - page {doc.metadata.get('page', 'unknown')}"
-        for doc in docs
-    ]
-    sources = list(set(sources))
-    answer = generate_answer(docs, request.query)
 
-    return {
-    "answer": answer,
-    "sources": sources
-    }
+    return StreamingResponse(
+        stream_answer(docs, request.query),
+        media_type="text/plain"
+    )
--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -1,23 +1,38 @@
 # app/services/llm_service.py
 from langchain_groq import ChatGroq
-
+from typing import AsyncGenerator
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
-    temperature=0
+    temperature=0,
+    streaming=True 
 )
 
-def generate_answer(context, query):
+async def stream_answer(context, query) -> AsyncGenerator[str, None]:
     combined_context = "\n".join([doc.page_content for doc in context])
-    
+
     prompt = f"""
-    Answer the question using the context below:
-    
+    Answer ONLY using the context.
+    If not found, say "Not found in provided documents".
+
     Context:
     {combined_context}
-    
+
     Question:
     {query}
     """
-    
-    response = llm.invoke(prompt)
-    return response.content
+
+    # stream answer
+    async for chunk in llm.astream(prompt):
+        content = getattr(chunk, "content", "")
+
+        if content:
+            yield str(content)  
+    yield "\n\nSources:\n"
+
+    sources = list({
+        f"{doc.metadata.get('source')} - page {doc.metadata.get('page')}"
+        for doc in context
+    })
+
+    for src in sources:
+        yield str(f"- {src}\n") 
