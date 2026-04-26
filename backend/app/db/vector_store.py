72db14c1a198c409985c14187f40ac6443dc5ba8
Pranav Patil <pranavpatil9765@gmail.com>
2026-04-26 11:09:54 +0530

    replaced faiss with pinecone

--- backend/app/db/vector_store.py
+++ backend/app/db/vector_store.py
@@ -1,8 +1,30 @@
 # app/db/vector_store.py
-from langchain_community.vectorstores import FAISS
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone , ServerlessSpec
+import os
+
 db = None
 
 def create_vector_store(chunks, embeddings):
     global db
-    db = FAISS.from_documents(chunks, embeddings)
+    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+    index_name = os.getenv("PINECONE_INDEX_NAME", "rag-index")
+    
+    # Create index if it doesn't exist
+    if index_name not in pc.list_indexes().names():
+        pc.create_index(
+            name=index_name,
+            dimension=1024,
+            metric="cosine",
+            spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+            )
+        )
+    
+    db = PineconeVectorStore.from_documents(
+        chunks, 
+        embeddings, 
+        index_name=index_name
+    )
     return db
